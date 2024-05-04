fc4d98e1008429752d3777918648f7d87ecf6229
shenjy <shenjy24@gmail.com>
2024-05-04 11:41:49 +0800

    feat:提示词

--- rag/rag_quickstart.py
+++ rag/rag_quickstart.py
@@ -5,6 +5,7 @@ from langchain_community.embeddings import OllamaEmbeddings
 from langchain_community.llms.ollama import Ollama
 from langchain_community.vectorstores import Chroma
 from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
@@ -27,7 +28,21 @@ embeddings = OllamaEmbeddings(model='llama3')
 vectorstore = Chroma.from_documents(documents=splits, embedding=embeddings)
 
 retriever = vectorstore.as_retriever()
-prompt = hub.pull("rlm/rag-prompt")
+# 使用模版
+# prompt = hub.pull("rlm/rag-prompt")
+
+# 自定义模板
+template = """Use the following pieces of context to answer the question at the end.
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+Use three sentences maximum and keep the answer as concise as possible.
+Always say "thanks for asking!" at the end of the answer.
+
+{context}
+
+Question: {question}
+
+Helpful Answer:"""
+prompt = PromptTemplate.from_template(template)
 
 
 def format_docs(docs):
